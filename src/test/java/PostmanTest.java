76047ea6f80a2394d90ad1120516e059fe5750cd
Sofya <sofa33158@gmail.com>
2023-04-17 20:25:50 +0300

    Initial commit

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,29 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+
+
+    @Test
+    void testAccept() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Data"))
+                .body(matchesJsonSchemaInClasspath("postman.shema.json"))
+        ;
+    }
+}
+
